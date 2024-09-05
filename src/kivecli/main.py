ce7a22b6fcd0824a29faf8231f9a53cc790d6b37
Vitaliy Mysak <vmysak@bccfe.ca>
2024-09-05 13:36:04 -0700

    make main.py shorter

--- src/kivecli/main.py
+++ src/kivecli/main.py
@@ -6,8 +6,7 @@ from typing import Sequence
 import kivecli.runkive as runkive
 import kivecli.zip as kiveclizip
 import kivecli.rerun as rerun
-from .usererror import UserError
-from .logger import logger
+from .mainwrap import mainwrap
 
 PROGRAMS = ["run", "zip", "rerun"]
 
@@ -62,20 +61,7 @@ def main(argv: Sequence[str]) -> int:
 
 
 def cli() -> None:
-    try:
-        rc = main(sys.argv[1:])
-        logger.debug("Done (%r).", rc)
-    except BrokenPipeError:
-        logger.debug("Broken pipe.")
-        rc = 1
-    except KeyboardInterrupt:
-        logger.debug("Interrupted.")
-        rc = 1
-    except UserError as e:
-        logger.fatal(e.fmt, *e.fmt_args)
-        rc = e.code
-
-    sys.exit(rc)
+    mainwrap(main)
 
 
 if __name__ == '__main__':
