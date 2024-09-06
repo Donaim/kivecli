ce4f3bb1251670a87b685c45ba4de27f01558170
Vitaliy Mysak <vmysak@bccfe.ca>
2024-09-06 15:43:47 -0700

    print found container name

--- src/kivecli/findrun.py
+++ src/kivecli/findrun.py
@@ -10,5 +10,7 @@ from .url import URL
 def find_run(kive: kiveapi.KiveAPI, run_id: int) -> Dict[str, object]:
     containerrun: Dict[str, object] = kive.endpoints.containerruns.get(run_id)
     url: str = str(containerrun["url"])
-    logger.debug("Found run with id %s at %s.", run_id, escape(URL(url)))
+    name: str = str(containerrun["name"])
+    logger.debug("Found run with id %s and name %s at %s.",
+                 run_id, escape(name), escape(URL(url)))
     return containerrun
