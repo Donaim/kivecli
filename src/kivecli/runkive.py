0afb109533b9d7542c43062f82107385eca0553b
Vitaliy Mysak <vmysak@bccfe.ca>
2025-05-05 14:32:35 -0700

    fix loglist fetch

--- src/kivecli/runkive.py
+++ src/kivecli/runkive.py
@@ -298,7 +298,7 @@ def main_logged_in(kive: kiveapi.KiveAPI,
                 filefilter=filefilter,
             )
 
-    log_list = kive.get(containerrun.log_list).json()
+    log_list = kive.get(containerrun.log_list.value).json()
     for log in log_list:
         if log["size"] == 0:
             logger.debug("Empty log of type %s.", escape(log["type"]))
