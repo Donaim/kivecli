8b83bcdadd3b16ee51066193dd9255ad18a26c96
Vitaliy Mysak <vmysak@bccfe.ca>
2025-05-23 18:47:04 -0700

    improve escaping

--- src/kivecli/escape.py
+++ src/kivecli/escape.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 from typing import NoReturn, Union
+import json
 
 from .pathorurl import PathOrURL
 from .url import URL
@@ -8,11 +9,11 @@ from .url import URL
 
 def escape(value: Union[PathOrURL, str]) -> str:
     if isinstance(value, Path):
-        return repr(str(value))
+        return json.dumps(str(value))
     elif isinstance(value, URL):
         return str(value)
     elif isinstance(value, str):
-        return repr(value)
+        return json.dumps(value)
     else:
         x: NoReturn = value
         assert x is None
